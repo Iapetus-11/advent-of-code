a0085af79c68b4396e287b619e8e6a2f97652a93
Milo Weinberg <iapetus011@gmail.com>
2021-12-01 10:04:43 -0500

    remove unnecessary spaces

--- 2021/1/part_2_golf.py
+++ 2021/1/part_2_golf.py
@@ -1,1 +1,1 @@
-s=[*map(int,open("input.txt").readlines()[:-1])];print(len([x for i in range(len(s)) if (x:=sum(s[i:i+3])>sum(s[i-1:i+2]))]))
+s=[*map(int,open("input.txt").readlines()[:-1])];print(len([x for i in range(len(s))if(x:=sum(s[i:i+3])>sum(s[i-1:i+2]))]))
