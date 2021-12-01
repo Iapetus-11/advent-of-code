cd6e8e3c91a8c7ec1b9883d2a147a43bc8de91f4
Milo Weinberg <iapetus011@gmail.com>
2021-12-01 10:21:04 -0500

    golf!

--- 2021/1/part_1_golf.py
+++ 2021/1/part_1_golf.py
@@ -1,1 +1,1 @@
-s=[*map(int,open("input.txt").readlines()[:-1])];print(sum(1 for i,x in enumerate(s,1)if x>[9**9,*s][i-1]))
+s=[*map(int,open("i").readlines()[:-1])];print(sum(1 for i,x in enumerate(s[1:],1)if x>s[i-1]))
