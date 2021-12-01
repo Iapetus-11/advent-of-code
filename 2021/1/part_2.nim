fd04706a178d97e32757dad19a6c7b0273af0053
Milo Weinberg <iapetus011@gmail.com>
2021-12-01 00:40:47 -0500

    add part 2 in nim!

---
+++ 2021/1/part_2.nim
@@ -0,0 +1,34 @@
+# nim doesn't like file that start with numbers, reasonable ig
+
+import std/[strutils, sequtils]
+
+let
+    input = readFile("input.txt")
+    lines: seq[string] = input.splitLines().toSeq
+
+var
+    increases = 0
+    last = -1
+    measurements = newSeq[int]()
+
+for line in lines:
+    try:
+        measurements.add(line.parseInt)
+    except ValueError:
+        discard
+
+last = measurements[0]
+
+proc sum(s: seq[int]): int =
+    for i in s:
+        result += i
+
+for i in 1 .. measurements.high:
+    let s = measurements[i .. min(i+2, measurements.high)].sum
+
+    if s > last:
+        increases += 1
+    
+    last = s
+
+echo increases
