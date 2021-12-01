9846e40fa2ea009ab35d69f924e3b34641b7b9a6
Milo Weinberg <iapetus011@gmail.com>
2021-12-01 03:12:39 -0500

    GOLF

--- 2021/1/part_1_golf.py
+++ 2021/1/part_1_golf.py
@@ -1,1 +1,1 @@
-with open("input.txt")as f:s=[*map(int,f.read()[:-1].split("\n"))];print(len([x for i,x in enumerate(s,1)if x>([9**9]+s)[i-1]]))
+s=[*map(int,open("input.txt").readlines()[:-1])];print(len([x for i,x in enumerate(s,1)if x>[9**9,*s][i-1]]))
--- 2021/1/part_2_golf.py
+++ 2021/1/part_2_golf.py
@@ -1,1 +1,1 @@
-print(len([x for i, x in enumerate([int(l) for l in open("input.txt", "r").readlines() if l != "\n"], start=1) if x > ([99**9] + [int(l) for l in open("input.txt", "r").readlines() if l != "\n"])[i-1]]))
+s=[*map(int,open("input.txt").readlines()[:-1])];print(len([x for i in range(len(s)) if (x:=sum(s[i:i+3])>sum(s[i-1:i+2]))]))
